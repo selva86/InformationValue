40cd2e2825bf168b7919b4bcd25a04b6799cb3f6
Selva Prabhakaran <selva86@gmail.com>
2015-08-04 15:00:30 +0530

    Updated 'optimalCutoff' documentation.

--- R/Main.R
+++ R/Main.R
@@ -502,6 +502,7 @@ IV <- function(X, Y, valueOfGood=1){
 #' @param actuals The actual binary flags for the response variable. It can take a numeric vector containing values of either 1 or 0, where 1 represents the 'Good' or 'Events' while 0 represents 'Bad' or 'Non-Events'.
 #' @param predictedScores The prediction probability scores for each observation. If your classification model gives the 1/0 predcitions, convert it to a numeric vector of 1's and 0's.
 #' @param optimiseFor The maximization criterion for which probability cutoff score needs to be optimised. Can take either of following values: "Ones" or "Zeros" or "Both" (default). If "Ones" is used, 'optimalCutoff' will be chosen to maximise detection of "One's". If 'Both' is specified, the probability cut-off that gives maximum Youden's Index is chosen.
+#' @param returnDiagnostics If TRUE, would return additional diagnostics such as 'sensitivityTable', 'misclassificationError', 'TPR', 'FPR' and 'specificity' for the chosen cut-off.
 #' @return The optimal probability score cutoff that maximises a given criterion. If 'returnDiagnostics' is TRUE, then the following items are returned in a list:
 #' \itemize{
 #'   \item optimalCutoff The optimal probability score cutoff that maximises a given criterion.
@@ -513,7 +514,8 @@ IV <- function(X, Y, valueOfGood=1){
 #' }
 #' @examples
 #' data('ActualsAndScores')
-#' optimProbCutoff(actuals=ActualsAndScores$Actuals, predictedScores=ActualsAndScores$PredictedScores, optimiseFor="Both", returnDiagnostics=TRUE)
+#' optimalCutoff(actuals=ActualsAndScores$Actuals,
+#' predictedScores=ActualsAndScores$PredictedScores, optimiseFor="Both", returnDiagnostics=TRUE)
 optimalCutoff <- function(actuals, predictedScores, optimiseFor="Both", returnDiagnostics=FALSE){
   # initialise the diagnostics dataframe to study the effect of various cutoff values.
   sequence <- seq(max(predictedScores), min(predictedScores), -0.01)
